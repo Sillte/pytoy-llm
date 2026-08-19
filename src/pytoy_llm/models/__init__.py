00c86ac11e2a2eecde19c28d89b747acc5ee3e55
sillte <innocent.konoha@gmail.com>
2026-08-19 20:48:30 +0900

    NITS

--- src/pytoy_llm/models/__init__.py
+++ src/pytoy_llm/models/__init__.py
@@ -1,4 +1,5 @@
 from pytoy_llm.models.llm_messages import LLMMessagesLike, LLMMessage  # noqa
 from pytoy_llm.models.llm_metas import LLMParam, LLMOutputMeta, LLMTokens  # noqa
+from pytoy_llm.models.agent_metas import UsageLimit  # noqa
 from pytoy_llm.models.llm_tools import LLMToolsLike  # noqa
 from pytoy_llm.models.parts import OpaquePart, Part, Role, PartAdapter  # noqa
